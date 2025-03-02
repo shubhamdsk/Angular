e4e9fc8d1fc7e91c53ea52ddcb2f403e08597507
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2025-03-02 20:16:16 +0530

    added routerLink and routerLinkActive

--- employee_management_system/src/app/app.component.ts
+++ employee_management_system/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RolesComponent } from "./component/roles/roles.component";
-import { MasterComponent } from "./component/master/master.component";
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RolesComponent, MasterComponent],
+  imports: [RouterOutlet, RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
